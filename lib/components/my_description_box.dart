df6c27c775de545d4d5766abe10b4a3e61ac3991
Kate <ekaterinatchubarkina@yandex.ru>
2025-04-17 12:47:06 +0500

    settings list

---
+++ lib/components/my_description_box.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyDescriptionBox extends StatelessWidget {
+  const MyDescriptionBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/components/my_current_location.dart';
 import 'package:fooddeliveryapp/components/my_drawer.dart';
+import 'package:fooddeliveryapp/components/my_sliver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,10 +14,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home")),
         drawer: MyDrawer(),
+      body: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled)=>[
+            MySliverAppBar(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Divider(indent: 25,
+                      endIndent: 25,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  //my current location
+                    MyCurrentLocation()
 
+                    //description box
+                ],
+                ),
+                title: Text("title")),
+          ],
+        body: Container(color: Colors.blue),
+      ),
     );
   }
 }
