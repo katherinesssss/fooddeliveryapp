2e33f621dc7a8666a060f25a561dc0746233c774
Kate <ekaterinatchubarkina@yandex.ru>
2025-04-19 00:05:39 +0500

    add new files,scroll menu

---
+++ lib/models/restaurant.dart
@@ -0,0 +1,342 @@
+import 'package:fooddeliveryapp/models/food.dart';
+
+class Restaurant {
+
+  //list of food menu
+  final List<Food> _menu = [
+    //burgers
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/food/burgers/Burger.png",
+    price:0.99,
+    category:FoodCategory.burgers,
+    availableAddons:[
+      Addon(name: "Extra cheese", price: 0.99),
+      Addon(name: "Bacon", price: 1.99),
+      Addon(name: "Avocado", price: 2.99),
+    ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    //salads
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+
+
+    //sides
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+
+    //desserts
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    //drinks
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+    Food(
+      name: "Classic Cheeseburger",
+      description: "A juicy beef patty with melted cheddar, lettuce,tomato, and a hint of onion and pickle.",
+      imagePath:"lib/images/burgers/cheese_burger.jpg",
+      price:0.99,
+      category:FoodCategory.burgers,
+      availableAddons:[
+        Addon(name: "Extra cheese", price: 0.99),
+        Addon(name: "Bacon", price: 1.99),
+        Addon(name: "Avocado", price: 2.99),
+      ],
+    ),
+  ];
+  /*
+  GETTERS
+   */
+List<Food> get menu=>_menu;
+/*
+OPERATIONS
+ */
+//add to cart
+
+//remove from cart
+
+//get total price of cart
+
+//get total number of items in cart
+
+// clear cart
+/*
+HELPERS
+  */
+
+//generate a receippt
+
+//format double value into money
+
+//format list of addons
+
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:fooddeliveryapp/components/my_current_location.dart';
+import 'package:fooddeliveryapp/components/my_description_box.dart';
 import 'package:fooddeliveryapp/components/my_drawer.dart';
 import 'package:fooddeliveryapp/components/my_sliver_app_bar.dart';
+import 'package:fooddeliveryapp/components/my_tab_bar.dart';
+import 'package:fooddeliveryapp/models/food.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,7 +13,22 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+  //tab controller
+  late TabController _tabController;
+
+  @override
+  void initState(){
+    //TODO implement initstate
+    super.initState();
+    _tabController = TabController(length: FoodCategory.values.length, vsync: this);
+  }
+
+  @override
+  void dispose(){
+    _tabController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +36,7 @@ class _HomePageState extends State<HomePage> {
       body: NestedScrollView(
           headerSliverBuilder: (context, innerBoxIsScrolled)=>[
             MySliverAppBar(
+                title: MyTabBar(tabController: _tabController),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
@@ -26,14 +45,31 @@ class _HomePageState extends State<HomePage> {
                       color: Theme.of(context).colorScheme.secondary,
                     ),
                   //my current location
-                    MyCurrentLocation()
+                    const MyCurrentLocation(),
 
                     //description box
+                    const MyDescriptionBox()
                 ],
-                ),
-                title: Text("title")),
+                )),
           ],
-        body: Container(color: Colors.blue),
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            ListView.builder(itemBuilder: (context,index)=>Text("first tab items"),
+            itemCount: 5,),
+            ListView.builder(itemBuilder: (context,index)=>Text("second tab items"),
+              itemCount: 5,),
+            ListView.builder(itemBuilder: (context,index)=>Text("third tab items"),
+              itemCount: 5,),
+            ListView.builder(itemBuilder: (context,index)=>Text("third tab items"),
+              itemCount: 5,),
+            ListView.builder(itemBuilder: (context,index)=>Text("third tab items"),
+              itemCount: 5,),
+            Text("flutter"),
+            Text("code"),
+
+          ],
+        ),
       ),
     );
   }
